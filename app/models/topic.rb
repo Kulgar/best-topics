84ccc040f46dda11bdcc726a49d184dfb939b536
Regis Millet <regmillet@ct2c.fr>
2017-09-08 13:32:34 +0200

    Added search field

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,11 @@
 class Topic < ApplicationRecord
+
+  def self.search(keywords)
+    if keywords.blank? # if keywords.nil? || keywords == ""
+      self
+    else
+      self.where('title LIKE ?', "%#{keywords}%")
+    end
+  end
+
 end

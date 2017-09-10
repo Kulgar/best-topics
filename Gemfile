448978774c37789dd46ccdeac5a39f61a9779041
Regis Millet <regmillet@ct2c.fr>
2017-09-10 17:56:38 +0200

    Added Annotate

--- Gemfile
+++ Gemfile
@@ -77,6 +77,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'highline' # https://github.com/JEG2/highline
+  gem 'annotate' # https://github.com/ctran/annotate_models
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/topic_answer.rb
+++ app/models/topic_answer.rb
@@ -1,2 +1,18 @@
 class TopicAnswer < ApplicationRecord
 end
+
+# ## Schema Information
+#
+# Table name: `topic_answers`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`content`**     | `text`             |
+# **`created_at`**  | `datetime`         | `not null`
+# **`id`**          | `integer`          | `not null, primary key`
+# **`published`**   | `boolean`          |
+# **`title`**       | `string`           |
+# **`updated_at`**  | `datetime`         | `not null`
+#

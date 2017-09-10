5516bbd0e44a396886c517a75ba198dcf42be665
Regis Millet <regmillet@ct2c.fr>
2017-09-10 17:56:38 +0200

    Added Annotate

--- test/models/topic_answer_test.rb
+++ test/models/topic_answer_test.rb
@@ -5,3 +5,19 @@ class TopicAnswerTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# ## Schema Information
+#
+# Table name: `topic_answers`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`content`**     | `text`             |
+# **`created_at`**  | `datetime`         | `not null`
+# **`id`**          | `integer`          | `not null, primary key`
+# **`published`**   | `boolean`          |
+# **`title`**       | `string`           |
+# **`updated_at`**  | `datetime`         | `not null`
+#
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -5,3 +5,19 @@ class TopicTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# ## Schema Information
+#
+# Table name: `topics`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`content`**     | `text`             |
+# **`created_at`**  | `datetime`         | `not null`
+# **`id`**          | `integer`          | `not null, primary key`
+# **`published`**   | `boolean`          | `default(TRUE)`
+# **`title`**       | `string`           |
+# **`updated_at`**  | `datetime`         | `not null`
+#
